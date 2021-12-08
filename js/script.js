238b8bb846ff1d40599d8c42936865387e456a1b
mark5041 <mark.beloso5041@gmail.com>
2021-12-08 21:12:23 +0100

    try remove listener

--- js/script.js
+++ js/script.js
@@ -5,14 +5,15 @@ function blacklist(num, max_mines)
     let max = num + 1;
     let min = 1;
     let i = 0;
-    let x = 0;
+    let row = 1;
+    let minesInrow = 0;
     let number;
     while(myArray.length < num * max_mines)
     {
         // ogni riga della griglia può contenere un massimo di mine
-        if(x < max_mines)
+        if(minesInrow < max_mines)
         {
-            x++;
+            minesInrow++;
             do
             {
                 number = Math.floor(Math.random() * (max - min)) + min;
@@ -21,13 +22,13 @@ function blacklist(num, max_mines)
             myArray[i] = number;
         }
         // se il numero massimo di mine viene raggiunto si resetta il contatore e si procede con la riga successiva
-        else    if(x == max_mines)
+        else    if(minesInrow == max_mines)
                 {
-                    x = 0;
+                    minesInrow = 0;
                     i--;
                     max += num;
                     min += num;
-                    
+                    row++;
                 }
         i++;
     }
@@ -39,44 +40,16 @@ function blacklist(num, max_mines)
             return -1;
         return 0;
     }));
-    
+
     return myArray;
 }
 
-// funzione per l'avvio del gioco
-function play()
+function columnNumber(array_number, dark_list, index)
 {
-    console.clear();
-    let game_over = document.querySelector(".game-over")
-
-    // reset generale
-    let row = document.querySelector(".row");
-    row.classList.remove("opacity-50");
-    game_over.classList.add("d-none");
-    row.innerHTML = '';
-
-    let diff = document.querySelector(".my-selection");
-    let col;
-    let mines;
-    let css_paper = document.documentElement.style;
-
-    // scelgo il numero di box e di mine per riga in base alla difficolta
-    switch(diff.value)
+    let i = index;
+    if(i > 1)
     {
-        case '1':
-            col = 10;
-            mines = 2;
-        break;
-        case '2':
-            col = 9;
-            mines = 3;
-        break;
-        case '3':
-            col = 7;
-            mines = 4;
-        break;
     }
-    css_paper.setProperty('--size', col);
 }
 
 
@@ -87,7 +60,7 @@ let diff_selector = document.querySelector(".my-btn");
 diff_selector.addEventListener('click', 
     function()
     {
-        play();
+        console.clear();
         let game_over = document.querySelector(".game-over")
 
         // reset generale
@@ -98,7 +71,7 @@ diff_selector.addEventListener('click',
 
         let diff = document.querySelector(".my-selection");
         let col;
-        let mines;
+        let mines = 1;
         let css_paper = document.documentElement.style;
 
         // scelgo il numero di box e di mine per riga in base alla difficolta
@@ -106,15 +79,12 @@ diff_selector.addEventListener('click',
         {
             case '1':
                 col = 10;
-                mines = 2;
             break;
             case '2':
                 col = 9;
-                mines = 3;
             break;
             case '3':
                 col = 7;
-                mines = 4;
             break;
         }
         css_paper.setProperty('--size', col);
@@ -126,54 +96,68 @@ diff_selector.addEventListener('click',
         for(let i = 1; i <= tot_box; i++)
         {
             let box = document.createElement("div");
-            box.classList.add("box", "dynamic");
+            box.classList.add("box");
             box.append(i);
             row.append(box);
 
-            box.addEventListener('click', 
-                function()
+            function click_trigger()
+            {
+                let resutl = document.querySelector(".game-over > h1");
+
+                // Seleziono il numero di box corretti già cliccati
+                let score = document.querySelectorAll(".box.bg-green");
+                let score_box = document.querySelector(".score");
+                if(!list.includes(parseInt(box.innerText)))
                 {
-                    let resutl = document.querySelector(".game-over > h1");
-
-                    // Seleziono il numero di box corretti già cliccati
-                    let score = document.querySelectorAll(".box.bg-green");
-                    let score_box = document.querySelector(".score");
-                    if(!list.includes(parseInt(box.innerText)) && box.classList.contains("dynamic"))
+                    this.classList.add("bg-green");
+                    console.log(this);
+                    // Aggiorno il numero di box corretti già cliccati
+                    score = document.querySelectorAll(".box.bg-green");
+                    // Se abbiamo selezionato tutti i box corretti uscirà la schermata di vittoria
+                    if(score.length == (col * col) - col * mines)
                     {
-                        this.classList.add("bg-green");
-
-                        // Aggiorno il numero di box corretti già cliccati
-                        score = document.querySelectorAll(".box.bg-green");
-                        // Se abbiamo selezionato tutti i box corretti uscirà la schermata di vittoria
-                        if(score.length == (col * col) - col * mines)
-                        {
-                            row.classList.add("opacity-50");
-                            game_over.classList.remove("d-none");
-                            resutl.innerHTML = '';
-                            resutl.innerHTML = 'YOU WIN';
-                            score_box.innerHTML = score.length;
-                        }
+                        row.classList.add("opacity-50");
+                        game_over.classList.remove("d-none");
+                        resutl.innerHTML = '';
+                        resutl.innerHTML = 'YOU WIN';
+                        score_box.innerHTML = score.length;
+                    }
+                }
+                else
+                {
+                    let box_created = document.querySelectorAll(".box");
+                    let bombs_exploded = document.querySelectorAll(".box.bg-red");
+                    let max_attemps = 2;
+                    if(bombs_exploded.length < max_attemps)
+                    {
+                        this.classList.add("bg-red");
+                        console.log(this);
                     }
                     else
                     {
-                        let box_created = document.querySelectorAll(".box.dynamic");
                         for(let i = 0; i < box_created.length; i++)
                         {
+                            console.log(box_created[i]);
+                            // Rendo non cliccabili tutti i box
+                            box_created[i].removeEventListener("click", click_trigger);
                             if(list.includes(parseInt(box_created[i].innerText)))
                             {
                                 // Rendo visibili tutte le mine
                                 box_created[i].classList.add("bg-red");
                             }
-                            // Rendo non cliccabili tutti i box
-                            box_created[i].classList.remove("dynamic");
+                            
                         }
+
                         // Schermata del Game Over
                         row.classList.add("opacity-50");
                         game_over.classList.remove("d-none");
-                        score_box.innerHTML = score.length;
+                        score_box.innerHTML = score.length;    
                     }
                 }
-            );
+                this.removeEventListener("click", click_trigger);
+            }
+
+            box.addEventListener('click', click_trigger);
         }
     }
 );
